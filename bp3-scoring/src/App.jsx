1794258461d6fd99a1cf7b297ccf0d45dbba6876
deviluthfitaningrum1 <luthfitaningrum@gmail.com>
2024-06-20 10:47:17 +0700

    logo

--- bp3-scoring/src/App.jsx
+++ bp3-scoring/src/App.jsx
@@ -10,21 +10,17 @@ function App() {
     <>
       <div>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="https://ukpppg.bppp.kemdikbud.go.id/peserta/assets/images/logo-menteri.svg" class="h-2rem md:h-4rem" alt=" " />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>SCORING</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
